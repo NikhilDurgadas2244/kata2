ed117bda85614e50a2dfafc2367520cb431f260b
NikhilDurgadas2244 <91554835+NikhilDurgadas2244@users.noreply.github.com>
2021-10-09 13:37:34 +0100

    Kata2 versión 5

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata2;
 
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- *
- * @author Usuario
- */
 public class Histogram<T> {
     private final T[] data;
     
--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,31 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata2;
 
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- *
- * @author Usuario
- */
 public class Kata2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
         
-        Integer[] data = {22, 11, 12, 34, 5, 6, 8, 10, 20, 30, 50, 100, 13, 14, 77};
+        String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
         Histogram histo = new Histogram(data);
-        Map<Integer,Integer> histogr = histo.getHistogram();
+        Map<String,Integer> histogr = histo.getHistogram();
         
 
-        for (Integer key : histogr.keySet()) {
+        for (String key : histogr.keySet()) {
             System.out.println(key + "==>" + histogr.get(key));
         }
     }
